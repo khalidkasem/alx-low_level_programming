bf1c07d9a2acf384c725fe90e97d690f08f29a7b
khalidkasem <khalidkasem1411@gmail.com>
2023-09-30 17:17:39 +0000

    0x08

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
-void _Puts_recursion(char *s);
-void _Print_rev_recursion(char *s);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -12,5 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif /* MAIN_H */
-
+#endif
